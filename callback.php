8f58018b7fb12afec0dd2b85fbdad697b20c6559
wangxiaomo <wangxiaomo@douban.com>
2014-03-03 10:01:40 +0800

    refactor

--- callback.php
+++ callback.php
@@ -3,16 +3,17 @@ include("./config.inc.php");
 include(UTILS_PATH . "/wechat.php");
 include(HANDLERS_PATH . "/handle_event.php");
 
-$revData = $wechat->getRev();
-$type = $revData->getRevType();
+$wechat = get_wechat();
+$wechat->getRev();
+$type = $wechat->getRevType();
 switch($type) {
     case Wechat::MSGTYPE_TEXT:
         $wechat->text("xx")->reply();
         break;
     case Wechat::MSGTYPE_EVENT:
-        $ret = handle_event($revData);
+        $ret = handle_event($wechat);
         if($ret){
-            $wechat->text(handle_event($revData))->reply();
+            $wechat->text(handle_event($wechat))->reply();
         }
         break;
     case Wechat::MSGTYPE_IMAGE:
--- utils/wechat.php
+++ utils/wechat.php
@@ -1,11 +1,14 @@
 <?php
 require_once(LIBS_PATH . "/wechat-php-sdk/wechat.class.php");
 
-$wechat_options = array(
-    "token" =>  APP_TOKEN,
-    "appid" =>  APP_ID,
-    "appsecret" =>  APP_SECRET,
-);
-$wechat = new Wechat($wechat_options);
-$wechat->valid();
+function get_wechat(){
+    $wechat_options = array(
+        "token" =>  APP_TOKEN,
+        "appid" =>  APP_ID,
+        "appsecret" =>  APP_SECRET,
+    );
+    $wechat = new Wechat($wechat_options);
+    $wechat->valid(true);
+    return $wechat;
+}
 ?>
